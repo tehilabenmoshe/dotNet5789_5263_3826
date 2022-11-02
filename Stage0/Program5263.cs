fa6a00bfa5dfb9ff40b9859f73c80280086026cd
תהילה בן משה <bmtehila@gmail.com>
2022-11-02 17:44:31 +0200

    initial code+welcome functions

---
+++ Stage0/Program5263.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace targil0
+{
+    partial class Program
+    {
+        static partial void Welcome5263()
+        {
+            Console.WriteLine("im also here");
+        }
+    }
+}
