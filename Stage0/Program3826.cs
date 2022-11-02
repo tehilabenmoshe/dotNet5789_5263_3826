6b56a9ecffbc7e3d82afc7cb83966bb995b60f60
מעיין עמר <maayanamar11.01@gmail.com>
2022-11-02 17:13:21 +0200

    initial code+ welcome fuction

---
+++ Stage0/Program3826.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome3826();
+            Welcome5263();
+            Console.ReadKey();
+        }
+        static partial void Welcome5263();  
+        private static void Welcome3826()
+        {
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0},welcome to my first console application", name);
+        }
+    }
+}
