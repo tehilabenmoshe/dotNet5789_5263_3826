b6d1930b03061a7e526b257f8601d17945da60a8
מעיין עמר <maayanamar11.01@gmail.com>
2023-01-08 12:17:02 +0200

    המשך תחילת 5

---
+++ PL/Manager/MainManagerPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PL.Manager
+{
+    /// <summary>
+    /// Interaction logic for MainManagerPage.xaml
+    /// </summary>
+    public partial class MainManagerPage : Page
+    {
+        public MainManagerPage()
+        {
+            InitializeComponent();
+
+        }
+
+     
+        private void ProductButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
